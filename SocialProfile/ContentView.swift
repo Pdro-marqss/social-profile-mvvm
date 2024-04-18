68d53953644f87fdcf0d54123d30aa8a042088c7
Pedro Marques <pdro.marqs2015@hotmail.com>
2024-04-18 00:17:49 -0300

    Carregando imagem por link externo

--- SocialProfile/ContentView.swift
+++ SocialProfile/ContentView.swift
@@ -8,12 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            AsyncImage(url: URL(string: "https://github.com/Pdro-marqss.png")) { imageState in
+                switch imageState {
+                case .empty:
+                    ProgressView()
+                case .success(let image):
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        .frame(height: 180)
+                case .failure:
+                    Image(systemName: "photo")
+                default:
+                    EmptyView()
+                }
+            }
+            Text("Pedro Marques")
         }
         .padding()
     }
