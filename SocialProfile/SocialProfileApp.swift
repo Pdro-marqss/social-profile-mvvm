0c29ec3e42344c75f341ded46a38b43cbcdba607
Pedro Marques <pdro.marqs2015@hotmail.com>
2024-04-17 23:43:55 -0300

    Initial Commit

---
+++ SocialProfile/SocialProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialProfileApp.swift
+//  SocialProfile
+//
+//  Created by Pedro Marques on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SocialProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
